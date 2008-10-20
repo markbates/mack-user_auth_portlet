70f17e63fa009b93b32b290b5b6bcf93349ee36f
Mark Bates <mark@markbates.com>
2008-10-20 16:59:28 -0400

    Did some more clean up.

--- config/configatron/default.rb
+++ config/configatron/default.rb
@@ -6,6 +6,8 @@ configatron.user_auth.emails.welcome.from = 'notifier@example.com'
 configatron.user_auth.bio_photo.path = Mack::Paths.images('bios')
 configatron.user_auth.bio_photo.thumb_size = 150
 
+configatron.user_auth.link_email_address = true
+
 begin
   require 'image_science'
   configatron.user_auth.use_image_science = true
